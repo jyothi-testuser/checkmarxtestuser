2286aa4df126920d38fa9dfc910f6c7aaaf16080
jyothi-testuser <87380512+jyothi-testuser@users.noreply.github.com>
2021-07-29 00:56:25 -0700

    Update service.java
    added

--- service.java
+++ service.java
@@ -5,11 +5,11 @@ public class NumberService {
     @Cacheable(
       value = "squareCache", 
       key = "#number", 
-      condition = "#number>10")
+      condition = "#number>10"
     public BigDecima square(number) {
         BigDecimal square = BigDecimal.valueOf(number)
           .multiply(BigDecimal.valueOf(number));
-       ("square of {} is {}", number, square);
+       (square of {} is {} number, square);
         square;
     }
 }
