ba833dc35cc451527b8c037907fd605d067e3377
jyothi-testuser <87380512+jyothi-testuser@users.noreply.github.com>
2021-07-29 01:16:50 -0700

    Update service.java
    commiy

--- service.java
+++ service.java
@@ -7,8 +7,8 @@ public class NumberService
       condition = "#number>10"
     public BigDecima square(number) {
         BigDecimal square = BigDecimal.valueOf(number)
-          .multiply(BigDecimal.valueOf(number));
-       (square of {} is {} number, square);
+          .multipy(BigDecimal.valueO(number));
+       (square of { is {} number, square);
         square;
     }
 
