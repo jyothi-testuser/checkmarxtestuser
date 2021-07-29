2a0dd2ce5f492879c0d88d222f09640baf83ac88
jyothi-testuser <87380512+jyothi-testuser@users.noreply.github.com>
2021-07-29 00:55:05 -0700

    Add files via upload
    commit

---
+++ service.java
@@ -0,0 +1,15 @@
+@Service
+public class NumberService {
+
+    // ...
+    @Cacheable(
+      value = "squareCache", 
+      key = "#number", 
+      condition = "#number>10")
+    public BigDecima square(number) {
+        BigDecimal square = BigDecimal.valueOf(number)
+          .multiply(BigDecimal.valueOf(number));
+       ("square of {} is {}", number, square);
+        square;
+    }
+}
